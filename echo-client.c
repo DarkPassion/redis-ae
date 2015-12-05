fba4373bd94b743edb75876b759159cebf5641aa
Evens <zhangzhifan.beat@gmail.com>
2015-12-05 12:11:14 +0800

    vim format file

--- echo-client.c
+++ echo-client.c
@@ -20,72 +20,72 @@ void readProc(struct aeEventLoop* eventLoop, int fd, void* clientdata, int mask)
 
 void writeProc(struct aeEventLoop* eventLoop, int fd, void* clientdata, int mask)
 {
-	
-	char* ptr = (char*)clientdata;
-	int len = strlen(ptr);
-	printf("writeProc -- eventLoop %p fd %d clientdata %p mask %d len %d\n", eventLoop, fd, clientdata, mask, len);
-	
-	anetWrite(fd, ptr, len);
-	free(ptr);
-	aeDeleteFileEvent(eventLoop, fd, AE_WRITABLE);
+
+        char* ptr = (char*)clientdata;
+        int len = strlen(ptr);
+        printf("writeProc -- eventLoop %p fd %d clientdata %p mask %d len %d\n", eventLoop, fd, clientdata, mask, len);
+
+        anetWrite(fd, ptr, len);
+        free(ptr);
+        aeDeleteFileEvent(eventLoop, fd, AE_WRITABLE);
 }
 
 void readProc(struct aeEventLoop* eventLoop, int fd, void* clientdata, int mask)
 {
-	printf(" client_read_proc -- fd %d  read_times %d\n",  fd, read_times++);
-	char read_buff[1024] = {0};
+        printf(" client_read_proc -- fd %d  read_times %d\n",  fd, read_times++);
+        char read_buff[1024] = {0};
 
-	int n_read = 0;
+        int n_read = 0;
 
-	//n_read = anetRead(fd, read_buff, sizeof(read_buff) -1);
-	n_read = recv(fd, read_buff, sizeof(read_buff), 0);
+        //n_read = anetRead(fd, read_buff, sizeof(read_buff) -1);
+        n_read = recv(fd, read_buff, sizeof(read_buff), 0);
 
-	
-	
-	if (n_read == 0) {
-		printf("server close socket -- \n");
-		aeDeleteFileEvent(eventLoop, fd, AE_READABLE);
-		close(fd);
-    fd = 0;
-	}
 
-  if (n_read == -1) {
-    printf("socket read error \n");
-    aeDeleteFileEvent(eventLoop, fd, AE_READABLE);
-    close(fd);
-    fd = 0; 
-  }
-	if (n_read <= 0) {
-		return ;
-	}
 
-	printf("n_read == %d %s \n", n_read, read_buff);
-	char* writebuff = (char*)malloc(n_read);
-	memset(writebuff, 0, n_read);
-	memcpy(writebuff, read_buff, n_read);
-	aeCreateFileEvent(eventLoop, fd, AE_WRITABLE, writeProc, writebuff);
+        if (n_read == 0) {
+                printf("server close socket -- \n");
+                aeDeleteFileEvent(eventLoop, fd, AE_READABLE);
+                close(fd);
+                fd = 0;
+        }
+
+        if (n_read == -1) {
+                printf("socket read error \n");
+                aeDeleteFileEvent(eventLoop, fd, AE_READABLE);
+                close(fd);
+                fd = 0; 
+        }
+        if (n_read <= 0) {
+                return ;
+        }
+
+        printf("n_read == %d %s \n", n_read, read_buff);
+        char* writebuff = (char*)malloc(n_read);
+        memset(writebuff, 0, n_read);
+        memcpy(writebuff, read_buff, n_read);
+        aeCreateFileEvent(eventLoop, fd, AE_WRITABLE, writeProc, writebuff);
 }
 
 
 int main()
 {
-	aeEventLoop* el = aeCreateEventLoop(1024);
+        aeEventLoop* el = aeCreateEventLoop(1024);
 
-	int fd = anetTcpConnect(myerr, "127.0.0.1", 8080);
+        int fd = anetTcpConnect(myerr, "127.0.0.1", 8080);
 
-	char writebuff[20] = {0};
-	const char * ptr = "hello worold !";
-	sprintf(writebuff, ptr, strlen(ptr));
-	anetWrite(fd, writebuff, strlen(ptr));
+        char writebuff[20] = {0};
+        const char * ptr = "hello worold !";
+        sprintf(writebuff, ptr, strlen(ptr));
+        anetWrite(fd, writebuff, strlen(ptr));
 
-	anetNonBlock(myerr, fd);
-	anetEnableTcpNoDelay(myerr, fd);
-	aeCreateFileEvent(el, fd, AE_READABLE, readProc, el);
+        anetNonBlock(myerr, fd);
+        anetEnableTcpNoDelay(myerr, fd);
+        aeCreateFileEvent(el, fd, AE_READABLE, readProc, el);
 
-	printf("main --- \n");
-	aeMain(el);
-	aeDeleteEventLoop(el);
+        printf("main --- \n");
+        aeMain(el);
+        aeDeleteEventLoop(el);
 
 
-	return 0;
+        return 0;
 }
--- echo-server.c
+++ echo-server.c
@@ -20,92 +20,92 @@ void acceptProc(struct aeEventLoop* eventLoop, int fd, void* clientdata, int mas
 
 void writeProc(struct aeEventLoop* eventLoop, int fd, void* clientdata, int mask)
 {
-	char* ptr = (char*)clientdata;
+        char* ptr = (char*)clientdata;
 
-	int len = strlen(ptr);
-	printf("writeProc -- eventLoop %p fd %d clientdata %p mask %d len %d\n", eventLoop, fd, clientdata, mask, len);
-	
-	anetWrite(fd, ptr, len);
-	free(ptr);
-	aeDeleteFileEvent(eventLoop, fd, AE_WRITABLE);
+        int len = strlen(ptr);
+        printf("writeProc -- eventLoop %p fd %d clientdata %p mask %d len %d\n", eventLoop, fd, clientdata, mask, len);
+
+        anetWrite(fd, ptr, len);
+        free(ptr);
+        aeDeleteFileEvent(eventLoop, fd, AE_WRITABLE);
 }
 
 void client_read_proc(struct aeEventLoop* eventLoop, int fd, void* clientdata, int mask)
 {
-	printf(" client_read_proc -- fd %d  read_times %d\n",  fd, read_times++);
-	char read_buff[1024] = {0};
+        printf(" client_read_proc -- fd %d  read_times %d\n",  fd, read_times++);
+        char read_buff[1024] = {0};
 
-	int n_read = 0;
+        int n_read = 0;
 
-	n_read = recv(fd, read_buff, sizeof(read_buff), 0);
-	//n_read = anetRead(fd, read_buff, sizeof(read_buff) -1);
-	
-	if (n_read == 0) {
-		printf("client socket close = \n");
-		aeDeleteFileEvent(eventLoop, fd, AE_READABLE);
-		close(fd);
-		fd = 0;
-		return;
-	}
+        n_read = recv(fd, read_buff, sizeof(read_buff), 0);
+        //n_read = anetRead(fd, read_buff, sizeof(read_buff) -1);
 
-	if (n_read == -1) {
-	  if (errno == EAGAIN) {
-        	n_read = 0;
-	  } else {
-		    printf("read socket error == \n");
-		    aeDeleteFileEvent(eventLoop, fd, AE_READABLE);
-        close(fd);
-        fd = 0;
-    	}
-  }
+        if (n_read == 0) {
+                printf("client socket close = \n");
+                aeDeleteFileEvent(eventLoop, fd, AE_READABLE);
+                close(fd);
+                fd = 0;
+                return;
+        }
 
-	printf("n_read == %d  %s \n", n_read, read_buff);
-	if (n_read <= 0) {
-		return;
-	}
-	
+        if (n_read == -1) {
+                if (errno == EAGAIN) {
+                        n_read = 0;
+                } else {
+                        printf("read socket error == \n");
+                        aeDeleteFileEvent(eventLoop, fd, AE_READABLE);
+                        close(fd);
+                        fd = 0;
+                }
+        }
 
-	char* writebuff = (char*)malloc(n_read);
-	memset(writebuff, 0, n_read);
-	memcpy(writebuff, read_buff, n_read);
-	aeCreateFileEvent(eventLoop, fd, AE_WRITABLE, writeProc, writebuff);
+        printf("n_read == %d  %s \n", n_read, read_buff);
+        if (n_read <= 0) {
+                return;
+        }
+
+
+        char* writebuff = (char*)malloc(n_read);
+        memset(writebuff, 0, n_read);
+        memcpy(writebuff, read_buff, n_read);
+        aeCreateFileEvent(eventLoop, fd, AE_WRITABLE, writeProc, writebuff);
 }
 
 
 void acceptProc(struct aeEventLoop* eventLoop, int fd, void* clientdata, int mask)
 {
 
-	printf(" eventLoop %p fd %d clientData %p int mast %d \n", eventLoop, fd, clientdata, mask);
-	char ip[20] = {0};
-	int port = 0;
-	int clientfd = anetTcpAccept(myerr, fd, ip, &port);
+        printf(" eventLoop %p fd %d clientData %p int mast %d \n", eventLoop, fd, clientdata, mask);
+        char ip[20] = {0};
+        int port = 0;
+        int clientfd = anetTcpAccept(myerr, fd, ip, &port);
 
-	if (clientfd == AE_ERR) {
-		printf("error acceptProc -- \n");
-		return;
-	}
+        if (clientfd == AE_ERR) {
+                printf("error acceptProc -- \n");
+                return;
+        }
 
-	printf("client info - ip %s port %d \n", ip, port);
-	
-	anetEnableTcpNoDelay(myerr, clientfd);
-	
-	int ret = anetNonBlock(myerr, clientfd);
-	if (ret == ANET_OK) {
-		printf("anetNonBlock-- succ\n");
-	}
-	aeCreateFileEvent(eventLoop, clientfd, AE_READABLE, client_read_proc, eventLoop);
+        printf("client info - ip %s port %d \n", ip, port);
+
+        anetEnableTcpNoDelay(myerr, clientfd);
+
+        int ret = anetNonBlock(myerr, clientfd);
+        if (ret == ANET_OK) {
+                printf("anetNonBlock-- succ\n");
+        }
+        aeCreateFileEvent(eventLoop, clientfd, AE_READABLE, client_read_proc, eventLoop);
 }
 
 int main()
 {
-	aeEventLoop* el = aeCreateEventLoop(1024);
-	int fd = anetTcpServer(myerr, 8080, "0.0.0.0");
+        aeEventLoop* el = aeCreateEventLoop(1024);
+        int fd = anetTcpServer(myerr, 8080, "0.0.0.0");
 
-	aeCreateFileEvent(el, fd, AE_READABLE, acceptProc, el);
+        aeCreateFileEvent(el, fd, AE_READABLE, acceptProc, el);
 
 
-	aeMain(el);
+        aeMain(el);
 
-	aeDeleteEventLoop(el);
-	return 0;
+        aeDeleteEventLoop(el);
+        return 0;
 }
